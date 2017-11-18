cb4ab129a6b93bab8d677072f2eebd77e7a7e079
Chauncey <goodstuden@qq.com>
2017-11-18 15:41:28 +0800

    hello world

---
+++ hello.go
@@ -0,0 +1,17 @@
+// hello
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	var s,sep string
+	for i:=1 ;i<len(os.Args);i++{
+		s+=sep+os.Args[i]
+		sep=""
+	}
+	fmt.Println(s)
+}
